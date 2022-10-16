83187746d51dcecefbca9dc635fccdb0aa4e321c
iitfsdrkm <107793637+iitfsdrkm@users.noreply.github.com>
2022-10-16 10:34:08 +0530

    Add files via upload

---
+++ customer-management/src/main/java/in/randip/crm/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package in.randip.crm.dao;
+
+import java.util.List;
+
+import in.randip.crm.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+
+}
